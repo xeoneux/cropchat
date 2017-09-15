47a12f7237d575fa38cd67686225296587663f3c
Aayush Kapoor <xeoneux@icloud.com>
2017-09-15 19:25:43 +0530

    Add hide menu function

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,13 @@
 <script>
 require('material-design-lite')
 export default {
-  name: 'app'
+  name: 'app',
+  methods: {
+    hideMenu: () => {
+      document.getElementsByClassName('mdl-layout__drawer')[0].classList.remove('is-visible')
+      document.getElementsByClassName('mdl-layout__obfuscator')[0].classList.remove('is-visible')
+    }
+  }
 }
 </script>
 
