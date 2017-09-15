60e07996397d7ac8071ecae2cdfd4c4c691d8a27
Aayush Kapoor <xeoneux@icloud.com>
2017-09-15 19:30:38 +0530

    Load data in home view

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -1,10 +1,38 @@
 <template>
-  <ul class="list">
-  </ul>
+  <div>
+    <div class="mdl-grid">
+      <div class="mdl-cell mdl-cell--3-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
+      <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-phone">
+        <div v-for="picture in this.pictures" class="image-card" @click="displayDetails(picture.id)">
+          <div class="image-card__picture">
+            <img :src="picture.url" />
+          </div>
+          <div class="image-card__comment mdl-card__actions">
+            <span>{{ picture.comment }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+    <router-link class="add-picture-button mdl-button mdl-js-button mdl-button--fab mdl-button--colored" to="/post">
+      <i class="material-icons">add</i>
+    </router-link>
+  </div>
 </template>
 
 <script>
-export default {}
+import data from '../data'
+export default {
+  methods: {
+    displayDetails (id) {
+      this.$router.push({ name: 'detail', params: { id: id } })
+    }
+  },
+  data () {
+    return {
+      'pictures': data.pictures
+    }
+  }
+}
 </script>
 
 <style scoped>
