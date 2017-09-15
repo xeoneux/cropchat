43c243deea2ec49cbf55f7f27efabf114b5950dd
Aayush Kapoor <xeoneux@icloud.com>
2017-09-15 18:42:38 +0530

    Insert material design lite

--- src/App.vue
+++ src/App.vue
@@ -11,12 +11,16 @@
 </template>
 
 <script>
+require('material-design-lite')
 export default {
   name: 'app'
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/icon?family=Material+Icons');
+@import url('https://code.getmdl.io/1.2.1/material.blue-red.min.css');
+
 body {
   margin: 0;
 }
