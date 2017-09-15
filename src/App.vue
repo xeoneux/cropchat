ca419005947a5632e71b657edfeff68e19828f93
Aayush Kapoor <xeoneux@icloud.com>
2017-09-15 19:11:20 +0530

    Add nav bar

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,21 @@
 <template>
-  <div id="app">
-    <header>
-      <span>Vue.js PWA</span>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <div class="mdl-layout__header-row">
+        <span class="mdl-layout-title">CropChat</span>
+      </div>
     </header>
-    <main>
-      <img src="./assets/logo.png" alt="Vue.js PWA">
-      <router-view></router-view>
+    <div class="mdl-layout__drawer">
+      <span class="mdl-layout-title">CropChat</span>
+      <nav class="mdl-navigation">
+        <router-link class="mdl-navigation__link" to="/" @click.native="hideMenu">Home</router-link>
+        <router-link class="mdl-navigation__link" to="/post" @click.native="hideMenu">Post a picture</router-link>
+      </nav>
+    </div>
+    <main class="mdl-layout__content">
+      <div class="page-content">
+        <router-view></router-view>
+      </div>
     </main>
   </div>
 </template>
