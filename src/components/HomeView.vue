6b0af4e745b09f05a3e0011501f9cdf16c6be6e7
Aayush Kapoor <xeoneux@icloud.com>
2017-09-15 19:36:06 +0530

    Add styles to image cards

--- src/components/HomeView.vue
+++ src/components/HomeView.vue
@@ -3,7 +3,7 @@
     <div class="mdl-grid">
       <div class="mdl-cell mdl-cell--3-col mdl-cell mdl-cell--1-col-tablet mdl-cell--hide-phone"></div>
       <div class="mdl-cell mdl-cell--6-col mdl-cell--4-col-phone">
-        <div v-for="picture in this.pictures" class="image-card" @click="displayDetails(picture.id)">
+        <div v-for="picture in this.pictures" v-bind:key="picture.id" class="image-card" @click="displayDetails(picture.id)">
           <div class="image-card__picture">
             <img :src="picture.url" />
           </div>
@@ -21,23 +21,50 @@
 
 <script>
 import data from '../data'
+
 export default {
   methods: {
     displayDetails (id) {
-      this.$router.push({ name: 'detail', params: { id: id } })
+      this.$router.push({ name: 'detail', params: { id } })
     }
   },
   data () {
     return {
-      'pictures': data.pictures
+      pictures: data.pictures
     }
   }
 }
 </script>
 
 <style scoped>
-.list {
+.add-picture-button {
+  position: fixed;
+  right: 24px;
+  bottom: 24px;
+  z-index: 998;
+}
+
+.image-card {
+  position: relative;
+  margin-bottom: 8px;
+}
+
+.image-card__picture>img {
   width: 100%;
-  padding: 0;
+}
+
+.image-card__comment {
+  position: absolute;
+  bottom: 0;
+  height: 52px;
+  padding: 16px;
+  text-align: right;
+  background: rgba(0, 0, 0, 0.5);
+}
+
+.image-card__comment>span {
+  color: #fff;
+  font-size: 14px;
+  font-weight: bold;
 }
 </style>
