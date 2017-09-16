82fe70c7d3a98a61c3d07a763b0e3528cc81a4d2
Aayush Kapoor <xeoneux@icloud.com>
2017-09-16 23:10:55 +0530

    Add vue resource dep

--- src/main.js
+++ src/main.js
@@ -2,12 +2,14 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import VueFire from 'vuefire'
+import VueResource from 'vue-resource'
 
 import App from './App'
 import router from './router'
 import firebase from './service/firebase'
 
 Vue.use(VueFire)
+Vue.use(VueResource)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
